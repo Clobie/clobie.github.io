d4c86bfc3de88780122a493ace5c42b98810f9e5
Clobie <calebtalexander@gmail.com>
2025-10-18 16:31:06 -0400

    .

--- orbdemo/index.service.worker.js
+++ orbdemo/index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1760817157|134086413';
+const CACHE_VERSION = '1760819403|1777250494';
 /** @type {string} */
-const CACHE_PREFIX = 'Godot Project-sw-cache-';
+const CACHE_PREFIX = 'orb-demo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
