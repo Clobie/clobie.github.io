3c4cc047dcb8977e78c9be9bedca6e62058f896a
Clobie <calebtalexander@gmail.com>
2026-05-29 01:23:59 -0400

    .

--- tentacle/index.service.worker.js
+++ tentacle/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779951490|33691229';
+const CACHE_VERSION = '1780032229|11073739300';
 /** @type {string} */
 const CACHE_PREFIX = '2d-framework-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
